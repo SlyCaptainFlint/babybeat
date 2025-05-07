86c97333413995d434d43fe7f101f5e39ae6c77c
olga <ozinoveva@gmail.com>
2025-05-06 20:30:26 -0700

    installable pwa

---
+++ baby-tracker-web/public/service-worker.js
@@ -0,0 +1,4 @@
+// Minimal service worker - just enough to make the PWA installable
+self.addEventListener('fetch', function(event) {
+  // This empty fetch handler is required for installation
+});
